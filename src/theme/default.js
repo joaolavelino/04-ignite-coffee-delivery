46334159367a293003bd29194a54f2da0348d19a
João Avelino <joaolavelino@gmail.com>
2025-04-09 10:24:01 -0300

    Initial Setup  - Img files  - Initial Dependencies

---
+++ src/theme/default.js
@@ -0,0 +1,23 @@
+export const defaultTheme = {
+  baseTitle: "#272221",
+  baseSubtitle: "#403937",
+  baseText: "#574F4D",
+  baseLabel: "#8d8686",
+  baseHover: "#d7d5d5",
+  baseButton: "#e6e5e5",
+  baseInput: "#ededed",
+  baseCard: "#f3f2f2",
+  baseBG: "#fafafa",
+  baseWhite: "#fff",
+
+  yellowDark: "#c47f17",
+  yellow: "#dbac2c",
+  yellowLight: "#f1e9c9",
+
+  purpleDark: "#4b2995",
+  purple: "#8047f8",
+  purpleLight: "#ebe5f9",
+
+  fontTitle: '"Baloo 2",sans-serif',
+  fontText: '"Roboto",sans-serif',
+};
